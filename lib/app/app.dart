185152bb38d325d886e6d16bea425b15d9999c7b
LohenYumnam <lohensana@gmail.com>
2021-10-21 11:33:22 +0530

    Changes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart';
-import '../../routes/routes.dart';
-
-class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  final _appRouter = AppRouter();
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerDelegate: _appRouter.delegate(),
-      routeInformationParser: _appRouter.defaultRouteParser(),
-    );
-  }
-}
+export './widget/app.dart';
